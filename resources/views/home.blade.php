eb713544306087b7bdf53e8dbd4c3b9521f7092b
marcovaccarini <quirkmode@yahoo.com>
2017-01-10 09:07:54 +0100

    fix home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -87,44 +87,43 @@
                     <div class="products-swiper">
                         <div class="swiper-container" data-autoplay="0" data-loop="0" data-speed="500" data-center="0" data-slides-per-view="responsive" data-xs-slides="2" data-int-slides="2" data-sm-slides="3" data-md-slides="4" data-lg-slides="5" data-add-slides="5">
                             <div class="swiper-wrapper">
-
                                 @foreach($featureds as $featured)
                                     <div class="swiper-slide">
-                                    <div class="paddings-container">
-                                        <div class="product-slide-entry">
-                                            <div class="product-image">
-                                                <img src="img/{{$featured->featured_image->filename}}" alt="{{$featured->product_name}}" />
-                                                @if($featured->new == 1)
-                                                    <div class="product-image-label type-1"><span>New</span></div>
-                                                @endif
-                                                @if($featured->custom_discount != null)
-                                                    <div class="product-image-label type-2"><span>{{number_format($featured->custom_discount), 0}}% OFF</span></div>
-                                                @endif
+                                        <div class="paddings-container">
+                                            <div class="product-slide-entry">
+                                                <div class="product-image">
 
-                                                <a class="top-line-a right"><i class="fa fa-heart"></i></a>
-                                                <div class="bottom-line">
-                                                    <a class="bottom-line-a"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                                    <img src="/img/{{$featured->featured_image->filename}}" alt="{{$featured->product_name}}" />
+
+                                                    @if($featured->custom_discount != null)
+                                                        <div class="product-image-label type-2"><span>{{number_format($featured->custom_discount), 0}}% OFF</span></div>
+                                                    @endif
+
+                                                    <a class="top-line-a right"><i class="fa fa-heart"></i> </a>
+                                                    <a href="{{$featured->path}}" class="top-line-a left quick-view">Details <i class="fa fa-search-plus"></i></a>
+                                                    <div class="bottom-line">
+                                                        <a class="bottom-line-a open-product" data-id="{{$featured->id}}"><i class="fa fa-shopping-cart"></i> Add to bag</a>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <a class="tag" href="#">{{$featured->category->title}}</a>
-                                            <a class="title" href="#">{{$featured->product_name}}</a>
-                                            <div class="price">
-                                                @if($featured->custom_discount != null)
-                                                    <div class="prev">${{$featured->price}}</div> |
-                                                    <div class="current">${{ number_format($featured->price - (($featured->price/100) * $featured->custom_discount), 2) }}</div>
-                                                @else
-                                                    <div class="current">${{$featured->price}}</div>
-                                                @endif
-                                            </div>
-                                            <div class="information-blocks">
-                                                <a class="button style-17" data-toggle="modal" href="ajax/product/{{$featured->id}}" data-target="#productSetailsModalAjax"><i class="fa fa-shopping-bag"></i>Add to bag</a>
+                                                <a class="tag" href="#">{{$featured->category->title}}</a>
+                                                {{--TODO: add link to category page--}}
+                                                <a class="title"  href="{{$featured->path}}">{{$featured->product_name}}</a>
+                                                <div class="price">
+                                                    @if($featured->custom_discount != null)
+                                                        <div class="prev">${{$featured->price}}</div> |
+                                                        <div class="current">${{ number_format($featured->price - (($featured->price/100) * $featured->custom_discount), 2) }}</div>
+                                                    @else
+                                                        <div class="current">${{$featured->price}}</div>
+                                                    @endif
+                                                </div>
+                                                <div class="information-blocks">
+                                                    <a class="button style-17 open-product" id="getUser" data-id="{{$featured->id}}"><i class="fa fa-shopping-bag"></i>Add to bag</a>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
                                 @endforeach
                             </div>
-
                             <div class="pagination"></div>
                         </div>
                     </div>
@@ -351,18 +350,21 @@
                                             <div class="product-slide-entry">
                                                 <div class="product-image">
 
-                                                    <img src="img/{{$promotion->featured_image->filename}}" alt="{{$promotion->product_name}}" />
+                                                    <img src="/img/{{$promotion->featured_image->filename}}" alt="{{$promotion->product_name}}" />
+
                                                     @if($promotion->custom_discount != null)
                                                         <div class="product-image-label type-2"><span>{{number_format($promotion->custom_discount), 0}}% OFF</span></div>
                                                     @endif
 
-                                                    <a class="top-line-a right"><i class="fa fa-heart"></i></a>
+                                                    <a class="top-line-a right"><i class="fa fa-heart"></i> </a>
+                                                    <a href="{{$promotion->path}}" class="top-line-a left quick-view">Details <i class="fa fa-search-plus"></i></a>
                                                     <div class="bottom-line">
-                                                        <a class="bottom-line-a"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                                        <a class="bottom-line-a open-product" data-id="{{$promotion->id}}"><i class="fa fa-shopping-cart"></i> Add to bag</a>
                                                     </div>
                                                 </div>
                                                 <a class="tag" href="#">{{$promotion->category->title}}</a>
-                                                <a class="title" href="#">{{$promotion->product_name}}</a>
+                                                {{--TODO: add link to category page--}}
+                                                <a class="title"  href="{{$promotion->path}}">{{$promotion->product_name}}</a>
                                                 <div class="price">
                                                     @if($promotion->custom_discount != null)
                                                         <div class="prev">${{$promotion->price}}</div> |
@@ -372,15 +374,13 @@
                                                     @endif
                                                 </div>
                                                 <div class="information-blocks">
-                                                    <a class="button style-17 open-product" data-toggle="modal"><i class="fa fa-shopping-bag"></i>Add to cart</a>
+                                                    <a class="button style-17 open-product" id="getUser" data-id="{{$promotion->id}}"><i class="fa fa-shopping-bag"></i>Add to bag</a>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 @endforeach
                             </div>
-                            <button data-toggle="modal" data-target="#view-modal" data-id="10" id="getUser" class="btn btn-sm btn-info"><i class="glyphicon glyphicon-eye-open"></i> View</button>
-
                             <div class="pagination"></div>
                         </div>
                     </div>
